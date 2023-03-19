44f53809e48f3f3f20295dbf9d5e9e941eb381c1
juanpabloct <juanptorres2003@gmail.com>
2023-03-18 20:01:53 -0500

    Clear compomponent separate in others components

--- src/pages/dataPokemons/[id].tsx
+++ src/pages/dataPokemons/[id].tsx
@@ -1,33 +1,18 @@
-import { useRouter } from "next/router";
+import Data from "@/components/data";
+import Navigation from "@/components/navigation";
+import FlexCol from "@/components/styles/flexCol";
 import FlexRow from "@/components/styles/flexRow";
-import { DatosPokemons } from "@/types/dataPokemons";
 import { Pagination } from "@mui/material";
 import axios from "axios";
-import { ChangeEvent, Dispatch, useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { ChangeEvent, useEffect, useState } from "react";
 const DataPokemons = () => {
-  const [page, setPage] = useState(1);
-
-  const [data, setData] = useState<DatosPokemons>();
-  useEffect(() => {
-    const getData = async () => {
-      const getDatos = await axios.get(
-        `https://api.pokemontcg.io/v2/cards?page=${page}&pageSize=10`
-      );
-      setData(getDatos.data);
-    };
-    getData();
-  }, [page]);
-
-  const handleChange = (event: ChangeEvent<unknown>, page: number) => {
-    setPage(page);
-  };
-  const paginas = data && Math.round(data.totalCount / 10) + 1;
+  const { id } = useRouter().query;
   return (
-    <FlexRow>
-      <Pagination count={paginas} page={page} onChange={handleChange} />
-
-      <Pagination count={paginas} page={page} onChange={handleChange} />
-    </FlexRow>
+    <FlexCol>
+      <Navigation />
+      {id && <Data id={+id} />}
+    </FlexCol>
   );
 };
 
