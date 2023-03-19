441b87a7ecb97167fe6440f935d6e94b61a9c310
juanpabloct <juanptorres2003@gmail.com>
2023-03-18 20:02:16 -0500

    importations innecesaries

--- src/pages/dataPokemons/[id].tsx
+++ src/pages/dataPokemons/[id].tsx
@@ -1,11 +1,7 @@
 import Data from "@/components/data";
 import Navigation from "@/components/navigation";
 import FlexCol from "@/components/styles/flexCol";
-import FlexRow from "@/components/styles/flexRow";
-import { Pagination } from "@mui/material";
-import axios from "axios";
 import { useRouter } from "next/router";
-import { ChangeEvent, useEffect, useState } from "react";
 const DataPokemons = () => {
   const { id } = useRouter().query;
   return (
