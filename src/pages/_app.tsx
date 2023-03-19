68166711030661ebbb384593b12d012daa04fa5a
juanpabloct <juanptorres2003@gmail.com>
2023-03-18 19:59:38 -0500

    Add redux-toolkit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
+import createStore from "@/reducers/store";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+const Store = createStore();
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={Store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
