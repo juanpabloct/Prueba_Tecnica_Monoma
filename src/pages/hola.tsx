0a5550615e5206d3d7d1b13beb4eaf0e059f10d4
juanpabloct <juanptorres2003@gmail.com>
2023-03-19 09:05:50 -0500

    delete component hola

--- src/pages/hola.tsx
+++
@@ -1,8 +0,0 @@
-import Navigation from "@/components/navigation";
-import { useRouter } from "next/router";
-
-const Hola = () => {
-  const route = useRouter();
-  return <Navigation />;
-};
-export default Hola;
