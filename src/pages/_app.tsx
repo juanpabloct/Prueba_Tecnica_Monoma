d593e2f269d6828e81bf6043dd7fae20da03ee69
juanpabloct <juanptorres2003@gmail.com>
2023-03-19 09:05:38 -0500

    Cange Store

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,10 @@
-import createStore from "@/reducers/store";
+import { store } from "@/reducers/store";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-const Store = createStore();
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Provider store={Store}>
+    <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
   );
